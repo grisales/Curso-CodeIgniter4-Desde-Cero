0ff6e5b4eeb973089d239c67f974d4fc8abe4f09
grisales <grisales@gmail.com>
2022-07-26 11:09:25 -0300

    Aula 31 - Migraciones Crear categories

---
+++ app/Database/Migrations/2022-07-25-195618_Movies.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Movies extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'movie_id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'movie_title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'movie_description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('movie_id', true);
+        $this->forge->createTable('movies');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('movies');
+    }
+}
