d1815995408450e0e7aba98a79f1da600e777150
grisales <grisales@gmail.com>
2022-08-10 21:49:44 -0300

    Aula 200 - Instalar PostMan

--- app/Controllers/RestMovie.php
+++ app/Controllers/RestMovie.php
@@ -18,4 +18,36 @@ class RestMovie extends ResourceController
         return $this->respond($this->model->findAll(),404,"Mecatiado en cositas");
         //
     }
+
+    /**
+     * ---:::[ FUNCIÓN CREAR REGISTRO ]:::---   
+     * 
+     * Esta función es para la RestApi
+     */
+
+    public function create()
+    {
+        $movie = new MovieModel();
+        
+        if($this->validate('movies'))
+        {
+            
+            $id = $movie->insert(
+            [
+                'movie_title' => $this->request->getPost('title'),
+                'movie_description' => $this->request->getPost('description'),
+                'category_id' => $this->request->getPost('category_id'),
+            ]);
+        
+            return $this->respond($this->model->find($id));
+            // return redirect()->to("dashboard/movie/edit/$id")->with('message', 'Película creada con éxito!');
+            
+        }
+        
+        return $this->respond("Errores");
+
+        // Errores
+        // return redirect()->back()->withInput();
+    }
+
 }
